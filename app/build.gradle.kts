0005ff892cd3d58e5fa4a38c689d47ab396c6919
sdboyapati <deepu.unity@gmail.com>
2025-01-05 17:30:14 -0500

    Unable to build the project when compileSdk is set to 34. So, updated the compileSdk to 35 and it worked. Now, both the compileSdk and targetSdk are targeting the latest android release (API 35)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.sdboyapati.echojournal"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sdboyapati.echojournal"
