dfafbe6353b00b4163476a1649cef4bb38335e9b
sdboyapati <deepu.unity@gmail.com>
2025-01-07 19:03:06 -0500

    Implemented Splash Screen for the app.

--- app/src/main/java/com/sdboyapati/echojournal/MainActivity.kt
+++ app/src/main/java/com/sdboyapati/echojournal/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sdboyapati.echojournal.ui.theme.EchoJournalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             EchoJournalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
